8f0a5da2ee6fa24b9d24e7214f35ea4608434c13
raHasan22 <r.ahmad.hasan.r@gmail.com>
2022-09-04 10:03:47 +0600

    news design add

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
-const loadCatagories = () =>{
+const loadCategories = () =>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayCategories(data.data.news_category))
 }
-loadCatagories();
+const displayCategories = (categories) =>{
+    const categoriesDiv = document.getElementById('category')
+    for(const category of categories){
+        console.log(category);
+        const categoryDiv = document.createElement('div')
+        categoryDiv.innerHTML = `
+        <button onclick="loadNews('${category.category_id}')" class="btn btn-outline-light text-dark fw-semibold">
+            ${category.category_name}
+          </button>
+        `;
+        categoriesDiv.appendChild(categoryDiv);
+    }
+}
+loadCategories();
+function loadNews(categoryId){
+    console.log(categoryId);
+}
