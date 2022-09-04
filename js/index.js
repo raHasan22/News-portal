54afa07df964d81f7fb703e15da31f348165c082
raHasan22 <r.ahmad.hasan.r@gmail.com>
2022-09-04 08:44:49 +0600

    added js file

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const loadCatagories = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+loadCatagories();
